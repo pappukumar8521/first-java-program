4487876fa39ebd10e0a1e20286018d33d550397c
pappukumar8521 <130996594+pappukumar8521@users.noreply.github.com>
2024-02-02 14:17:51 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class First{
+    public static void main(String args[]){
+        int age=15;
+        if(age>=18)
+        {
+            System.out.println("adult,drive,vote");
+        }
+            else{
+                System.out.println("not adult");
+            }
+        }
+    }
+
+
+
+
